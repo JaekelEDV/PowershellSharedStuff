6bfe9364d18874b6d175e55f64952b986b7954f9
@JaekelEDV <JaekelEDV@users.noreply.github.com>
2019-09-28 23:58:22 +0200

    Create NestedVirtualization.ps1

---
+++ NestedVirtualization.ps1
@@ -0,0 +1,15 @@
+﻿#Nested Virtualization
+$VMName = 'test'
+#Machine has to be off
+Stop-VM -Name $VMName
+#Make it believe it has virtualization capabilities
+Set-VMProcessor -VMName $VMName -ExposeVirtualizationExtensions $true
+#Dynamic Memory has to be disabled
+Set-VMMemory -VMName $VMName -DynamicMemoryEnabled $false
+#MAC-Address Spoofing has to be on
+Get-VMNetworkAdapter -VMName $VMName | select MacAddressSpoofing
+Get-VMNetworkAdapter -VMName $VMName | Set-VMNetworkAdapter -MacAddressSpoofing On
+#Bring back the VM
+Start-VM -Name $VMName
+#Inside the VM install the Hyper-V-Role
+Add-WindowsFeature -Name Hyper-V -IncludeAllSubFeature -IncludeManagementTools
