73af7715517b7af46d56be2b574db60984a146ab
@JaekelEDV <JaekelEDV@users.noreply.github.com>
2020-02-20 21:44:57 +0100

    Create Get-IPInfo.ps1

---
+++ Get-IPInfo.ps1
@@ -0,0 +1,11 @@
+﻿Function Get-IPInfo {
+  param (
+    [Parameter(Mandatory=$true,Helpmessage='Enter a valid IP Address')]
+    [string] $IP
+  )
+  Invoke-RestMethod -Uri http://ipinfo.io/$ip/json
+}
+
+
+
+  
