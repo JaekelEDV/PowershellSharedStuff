69683b366ec26676893e1ec24ca49e489367e67f
@JaekelEDV <JaekelEDV@users.noreply.github.com>
2021-02-21 21:34:12 +0100

    Create Repair-UpdateServices.ps1

---
+++ Repair-UpdateServices.ps1
@@ -0,0 +1,69 @@
+<#
+.SYNOPSIS
+    Repair-UpdateServices.ps1 stops Windows Update relevant services and renames relevant folders.
+.DESCRIPTION
+    Repair-UpdateServices.ps1 can help if Windows Update throws any errors or updates simply
+    won't finish installing.
+    Repair-UpdateServices.ps1 stops the following services:
+        wuauserv - Automatic Updates service
+        cryptSvc - Cryptographic Services
+        BITS - Background Intelligent Transfer Service
+        msiserver - Windows Installer service
+    Repair-UpdateServices.ps1 renames the following folders:
+        "C:\Windows\SoftwareDistribution"
+        "C:\Windows\System32\catroot2"
+    Then the above services are started again.
+    A reboot is recommended, but has to be confirmed.
+.EXAMPLE
+    PS C:\> Repair-UpdateServices.ps1
+    Simply execute the ps1-script.
+.LINK
+    https://support.microsoft.com/en-us/windows/troubleshoot-problems-updating-windows-10-188c2b0f-10a7-d72f-65b8-32d177eb136c
+.NOTES
+    File Name: Repair-UpdateServices.ps1
+    Author: Oliver Jaekel | oj@jaekel-edv.de | @JaekelEDV | https://github.com/JaekelEDV
+#>
+#Requires -RunAsAdministrator
+
+#region Function stopping the services
+function Repair-UpdateServices {
+
+    [CmdletBinding()]
+    param ()
+    $svc = Get-Service -Name "wuauserv", "cryptSvc", "BITS", "msiserver"
+    $svc |
+    Where-Object{ $_.status -eq 'Running' } |
+    Stop-Service -Force
+    $svc
+}
+#endregion
+
+#region Function renaming the folders
+function Rename-MyFolders {
+    [CmdletBinding()]
+    param ()
+    $SoftwareDistribution = "C:\Windows\SoftwareDistribution"
+    $catroot2 = "C:\Windows\System32\catroot2"
+    Rename-Item $SoftwareDistribution -NewName "SoftwareDistribution.old"
+    Rename-Item $catroot2 -NewName "catroot2.old"
+}
+#endregion
+
+#region Function starting the services.
+function Start-MyUpdateServices {
+    [CmdletBinding()]
+    param ()
+    $svc = Get-Service -Name "wuauserv", "cryptSvc", "BITS", "msiserver"
+    $svc |
+    Where-Object{ $_.status -eq 'Stopped' } |
+    Start-Service -Force
+    $svc
+}
+#endregion
+
+#region And action!
+Stop-MyUpdateServices
+Rename-MyFolders
+Start-MyUpdateServices
+Restart-Computer -Confirm:$true
+#endregion
