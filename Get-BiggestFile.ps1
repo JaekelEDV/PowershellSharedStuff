683c3e2bcd21481a26acb49123d0cea3d851a694
@JaekelEDV <JaekelEDV@users.noreply.github.com>
2023-03-17 23:07:05 +0100

    Update Get-BiggestFile.ps1

--- Get-BiggestFile.ps1
+++ Get-BiggestFile.ps1
@@ -1,2 +1,9 @@
-$Props = 'Name', @{Name = 'Size in GB'; Expression = {$_.Length / 1GB -as [int]}}, @{Name = 'Size in MB'; Expression = {$_.Length / 1MB -as [int]}}, 'Directory'
-Get-ChildItem -Recurse | Sort -Descending -Property Length | select -First 5 $Props | Format-List
+$props = 'Name',
+@{Name = 'Size in GB'; Expression = { $_.Length / 1GB -as [int] } },
+@{Name = 'Size in MB'; Expression = { $_.Length / 1MB -as [int] } },
+'Directory'
+Get-ChildItem -Recurse |
+Sort-Object -Descending -Property Length |
+Select-Object -First 5 $props |
+#Format-List
+Format-Table
